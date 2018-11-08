17e8167d8727eea4ff1e3a572feb1adc969b936b
Brandon Masterson <masterbs@alfredstate.edu>
2018-11-08 04:15:21 -0500

    brought in static assets and properly routed them

---
+++ va/static/js/init.js
@@ -0,0 +1,9 @@
+(function($){
+  $(function(){
+
+    //$('.sidenav').sidenav();
+    //$('.carousel').carousel();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
